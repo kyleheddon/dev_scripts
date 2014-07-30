c06579417592e8ac7cabb7b9c8d0e1eb5bdccbf0
Kyle Heddon <ktheddon@gmail.com>
2014-07-30 11:06:45 -0400

    clean

---
+++ delete_branches.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+def get_branch_from_user
+  print "Enter the name of the branch to delete, or press Enter to quit: "
+  gets.chomp
+end
+
+def delete(branch)
+  `git branch -D #{branch}`
+  print "branch, #{branch}, deleted\n"
+end
+
+def list_branches
+  print "\nGit branches:\n"
+  puts `git branch`
+  puts
+end
+
+########################################
+# Start
+########################################
+
+while(true) do
+  list_branches
+  branch = get_branch_from_user
+  
+  break if branch == ''
+
+  delete branch
+end 
