90ab4c48d945a798f5098592e368bbe200e4868c
Kyle Heddon <ktheddon@gmail.com>
2014-08-01 11:26:08 -0400

    clean remove_comments.rb

--- scripts/remove_comments.rb
+++ scripts/remove_comments.rb
@@ -1,28 +1,18 @@
 #!/usr/bin/env ruby
 
-def read_file(file_name)
-  i = 1
+def read_lines(file_name)
   lines = []
   file = File.new(file_name, "r")
   while (line = file.gets)
     stripped_line = line.strip
     lines << line unless stripped_line == '' || stripped_line[0] == '#'
-    # if i == 9
-    #   puts stripped_line.chomp[0] == ' '
-    #   puts stripped_line 
-    # end
-
-    i += 1
   end
   file.close
   lines
 end
 
 file_name = ARGV[0]
+contents = read_lines(file_name).join().strip
 
-contents = read_file(file_name).join()
-puts 'contents: '
-puts contents
 `echo '#{contents}' > #{file_name}`
-
-puts 'done'
+`cat #{file_name}`
